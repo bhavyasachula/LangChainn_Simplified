aee7ecef3e95d2cfc923b1a1b427afd5578149e0
bhavya sachula <bhavyasachula07@gmail.com>
2026-04-09 11:56:47 +0530

    program:how chain works

--- chain/chain.py
+++ chain/chain.py
@@ -1,15 +1,18 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
+from langchain_core.output_parsers import StrOutputParser # stroutputparser is function 
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 load_dotenv()
 model = ChatGroq(model="openai/gpt-oss-120b")
 
 prompttemplate = ChatPromptTemplate.from_messages(
-    [("system","You are a comedian who tells a joke on{topic}"),
+    [("system","You are a comedian who tells a joke on {topic}"),
     ("human","tell me {joke_count} joke. ")
      ]
 )
+#chain 
+chain = prompttemplate | model | StrOutputParser()
+result = chain.invoke({"topic":'water','joke_count':2})
 
-chain = prompttemplate.invoke({"topic":"lawyer","joke_count":2})
-print(chain)
+print(result)
