94c5a8448f1c7d9c274f37eeaed294bf4a6d9ed4
bhavya sachula <bhavyasachula07@gmail.com>
2026-04-17 22:56:49 +0530

    A gettime agent

--- agent_and_tools/basic_agent_and_tool.py
+++ agent_and_tools/basic_agent_and_tool.py
@@ -1,8 +1,40 @@
-import os
 from dotenv import load_dotenv
 from langsmith import Client
-from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain_classic.agents import create_react_agent,AgentExecutor
-from langchain.tools import tool
+from langchain_classic.agents import create_react_agent, AgentExecutor
+from langchain_classic.tools import Tool
+
 load_dotenv()
+
+llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
+client = Client()
+prompt = client.pull_prompt("hwchase17/react")
+
+def get_datetime(*args, **kwargs):
+    import datetime
+    return str(datetime.datetime.now())
+
+# ✅ Correct tool format for react agent
+tools = [
+    Tool(
+        name="time",
+        func=get_datetime,
+        description="Useful for when you need to know the current time"
+    )
+]
+
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=prompt,
+)
+
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+
+response = agent_executor.invoke({"input": "What time is it?"})
+
+print("response:", response["output"])
