15d6dbd78e76a8132a41bf8ab727595ba9e5c9ef
bhavya sachula <bhavyasachula07@gmail.com>
2026-04-18 12:35:58 +0530

    Fix:bug of repeating loop of agent

--- agent_and_tools/multiple_tools_agent.py
+++ agent_and_tools/multiple_tools_agent.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
 client = Client()
-prompt = client.pull_prompt("hwchase17/react")
+prompt = client.pull_prompt("hwchase17/structured-chat-agent")
 
 def get_datetime(*args, **kwargs):
     import datetime
@@ -17,18 +17,20 @@ def get_datetime(*args, **kwargs):
     return str(now)
 
 def wikipedia_summary(query):
-    from wikipedia import summary
-    result = summary(query,sentence=3)
-    return(result)
+    try:
+        from wikipedia import summary
+        return(summary(query,sentences=2))
+    except:
+        return("I couldn't find any information on that topic.")
 
 tools = [
     Tool(
-        name="time",
+        name="get_time",
         func=get_datetime,
         description="Useful for when you need to know the current time"
     ),
     Tool(
-        name="wikipedia",
+        name="search_wikipedia",
         func=wikipedia_summary,
         description="to find the information about any topic"
     )
@@ -54,18 +56,16 @@ agent_executor = AgentExecutor.from_agent_and_tools(
 initialmsg = "You are an helpfull assistant that provides an answers if u dont know the answer just say i dont know";
 memory.chat_memory.add_message(SystemMessage(content=initialmsg));
 
+print("Chat with Ai");
 while True:
-    print("Chat with Ai");
     user_input=input("You :");
 
     if user_input.lower() == "exit":
         break;
-
-    memory.chat_memory.add_message(HumanMessage(content=user_input));
-
+    
     response = agent_executor.invoke({"input":user_input});
     print("Ai :",response["output"]);
-    memory.chat_memory.add_message(AIMessage(content=response["output"]));
+   
     
 
   
